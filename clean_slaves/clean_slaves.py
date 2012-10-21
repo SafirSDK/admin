6342d6fd0fc5d49f20b00809ccf88f86ced7eed6
lars.hagstrom@consoden.se <lars.hagstrom@consoden.se@9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973>
2012-10-21 22:25:21 +0000

    a small step on the way to making a slave cleanup script
    git-svn-id: http://svn.code.sf.net/p/safir/svn/admin@717 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

---
+++ clean_slaves/clean_slaves.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+###############################################################################
+#
+# Copyright Saab AB, 2012 (http://www.safirsdk.com)
+#
+# Created by: Lars Hagstrom (lars@foldspace.nu)
+#
+###############################################################################
+#
+# This file is part of Safir SDK Core.
+#
+# Safir SDK Core is free software: you can redistribute it and/or modify
+# it under the terms of version 3 of the GNU General Public License as
+# published by the Free Software Foundation.
+#
+# Safir SDK Core is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Safir SDK Core.  If not, see <http://www.gnu.org/licenses/>.
+#
+###############################################################################
+from __future__ import print_function
+import os, sys
+
+BASE = os.environ.get("BASE")
+if BASE is None:
+    print("Failed to find Jenkins base directory, exiting")
+    sys.exit(1)
+if not os.path.isdir(BASE):
+    print(BASE, "does not appear to be a directory")
+    sys.exit(1)
+print("Changing directory to", BASE)
+os.chdir(BASE)
+contents = os.listdir(".")
+print (contents)
