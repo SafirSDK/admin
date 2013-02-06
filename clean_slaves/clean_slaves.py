e5f3cf278b4eaccaa74d0fd1fa05c9e3e7a55f5c
lars.hagstrom@consoden.se <lars.hagstrom@consoden.se@9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973>
2013-02-06 14:56:33 +0000

    bugfix
    git-svn-id: http://svn.code.sf.net/p/safir/svn/admin@1231 9dd7cf02-b8c9-4ff8-a4a5-e7fda3bdd973

--- clean_slaves/clean_slaves.py
+++ clean_slaves/clean_slaves.py
@@ -57,7 +57,7 @@ def onerror(function, path, excinfo):
     try:
         for d in splitpath(path):
             print("processing",d)
-            if os.path.is_file(d):
+            if os.path.isfile(d):
                 print("Would like to remove",d)
                 #                os.chmod(d, stat.S_IWRITE)
                 #                os.unlink(d)
